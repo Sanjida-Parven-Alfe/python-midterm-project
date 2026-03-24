ee0de4c70d78132e0f84ef04ae6de6c6d3b1d9cf
Hridrita <moonsaha721@gmail.com>
2026-03-25 01:16:21 +0600

    added ship movement

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ class Game:
 		self.player = pygame.sprite.GroupSingle(player_sprite)
 		
 	def run(self):
-	    self.player.draw(screen)
-		
-
-
+		self.player.update()
+		self.player.draw(screen)
+	
+	
+    
 
 if __name__ == '__main__':
 	pygame.init()
