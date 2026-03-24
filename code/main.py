99d0c2153761b84e5c3ba5b3d4953975fc8b6926
Hridrita <moonsaha721@gmail.com>
2026-03-24 23:43:38 +0600

    created a folder

--- code/main.py
+++ code/main.py
@@ -1,11 +1,19 @@
 import pygame, sys
 
+class Game:
+	def __init__(self):
+		pass
+	
+    def run(self):
+	    pass
+
 if __name__ == '__main__':
 	pygame.init()
 	screen_width = 600
 	screen_height = 600
 	screen = pygame.display.set_mode((screen_width,screen_height))
 	clock = pygame.time.Clock()
+	game = Game()
 
 	while True:
 		for event in pygame.event.get():
@@ -15,6 +23,7 @@ if __name__ == '__main__':
 			
 
 		screen.fill((30,30,30))
+		game.run()
 		
 			
 		pygame.display.flip()
