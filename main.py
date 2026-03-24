cee5fd52a0874c678b6242b12ea891d9dc17d175
Hridrita <moonsaha721@gmail.com>
2026-03-24 23:49:01 +0600

    created player sprite

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame, sys
+import Player from player
+
+class Game:
+	def __init__(self):
+		player_sprite = Player((300,300))
+		self.player = pygame.sprite.GroupSingle()
+    def run(self):
+	    pass
+
+if __name__ == '__main__':
+	pygame.init()
+	screen_width = 600
+	screen_height = 600
+	screen = pygame.display.set_mode((screen_width,screen_height))
+	clock = pygame.time.Clock()
+	game = Game()
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			
+
+		screen.fill((30,30,30))
+		game.run()
+		
+			
+		pygame.display.flip()
+		clock.tick(60)
