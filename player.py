7f6a8ceeaa666a7f4f8b29063c06e61a7c245900
Hridrita <moonsaha721@gmail.com>
2026-03-25 03:54:47 +0600

    modified shooter ship

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from laser import Laser
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,constraint,speed):
 		super().__init__()
-		self.image = pygame.image.load('graphics/player.png').convert_alpha()
+		self.image = pygame.image.load('graphics/player-1.png').convert_alpha()
 		self.rect = self.image.get_rect(midbottom = pos)
 		self.speed = speed
 		self.max_x_constraint = constraint
