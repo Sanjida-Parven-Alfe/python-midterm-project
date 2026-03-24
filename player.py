39725f5b2a67ae89d5afbc318bba8f409921ae8b
Hridrita <moonsaha721@gmail.com>
2026-03-25 01:47:51 +0600

    modified

--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 import pygame 
+
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos):
+	def __init__(self,pos,constraint,speed):
 		super().__init__()
-		self.image = pygame.image.load('../python-midterm-project/graphics/player.png').convert_alpha()
+		self.image = pygame.image.load('graphics/player.png').convert_alpha()
 		self.rect = self.image.get_rect(midbottom = pos)
-		self.speed = 5
+		self.speed = speed
+		self.max_x_constraint = constraint
 	def get_input(self):
 		keys = pygame.key.get_pressed()
 
@@ -12,5 +14,11 @@ class Player(pygame.sprite.Sprite):
 			self.rect.x += self.speed
 		elif keys[pygame.K_LEFT]:
 			self.rect.x -= self.speed
+	def constraint(self):
+		if self.rect.left <= 0:
+			self.rect.left = 0
+		if self.rect.right >= self.max_x_constraint:
+			self.rect.right = self.max_x_constraint
 	def update(self):
 		self.get_input()
+		self.constraint()
