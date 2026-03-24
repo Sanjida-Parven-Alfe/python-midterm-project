009369c3e6c4b77714ada3aefa46238649232265
Hridrita <moonsaha721@gmail.com>
2026-03-25 03:09:43 +0600

    created a obstacle

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import pygame, sys
 from player import Player
+import obstacle
 
 class Game:
 	def __init__(self):
+		#player setup
 		player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
 		self.player = pygame.sprite.GroupSingle(player_sprite)
+		
+        #obtacle setup
+		self.shape = obstacle.shape
+		self.block_size = 6
+		self.blocks = pygame.sprite.Group()
+		self.obstacle_amount = 4
+		self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+		self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 15, y_start = 480)
+		
+	def create_obstacle(self, x_start, y_start,offset_x):
+		for row_index, row in enumerate(self.shape):
+			for col_index,col in enumerate(row):
+				if col == 'x':
+					x = x_start + col_index * self.block_size + offset_x
+					y = y_start + row_index * self.block_size
+					block = obstacle.Block(self.block_size,(241,79,80),x,y)
+					self.blocks.add(block)
 		
 		
 	def run(self):
